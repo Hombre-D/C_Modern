86bfbc1d94ba24b16c0bf27656f62c015e17dddc
Hombre-D <danjelnygard@hotmail.com>
2018-03-06 21:08:47 -0600

    March 6

--- reverse.c
+++ reverse.c
@@ -1,19 +1,19 @@
-//Reverses a series of numbers.
+//Reverses a series of numbers. Modified for exercise 8-5
 
 #include <stdio.h>
-
 #define N 10
 
 int main()
 {
 	int a[N], i;
+	#define SIZE (sizeof(a) / sizeof(a[0]))
 
 	printf("Enter %d numbers: ", N);
-	for (i = 0; i < N; i++)
+	for (i = 0; i < SIZE; i++)
 		scanf("%d", &a[i]);
 
 	printf("In reverse order: ");
-	for (i = N - 1; i >=0; i--)
+	for (i = SIZE - 1; i >=0; i--)
 		printf(" %d", a[i]);
 	printf("\n");
 
